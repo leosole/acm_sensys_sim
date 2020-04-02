1df667f903567b208d3a2a8b0235b2a5c95b4425
leosole <leosole@gmail.com>
2020-04-02 12:15:17 -0300

    Delete soundTest.py

--- soundTest.py
+++
@@ -1,7 +0,0 @@
-import os
-import platform
-syst = platform.system()
-if syst == 'Linux':
-    os.system('spd-say "your program has finished"')
-elif syst == 'Darwin':
-    os.system('say "your program has finished"')
